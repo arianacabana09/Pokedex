1ef4b72ca21eeb45739e954f43f56698d961d815
Ariana Cabana <acabana.0904@gmail.com>
2017-06-15 15:19:30 -0500

    probando con el console.log

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,14 @@ $( ()=>{
   // $.getJSON("assets/js/endpoint-all.js", function(data) {
   //   console.log(data);
 
-  var json = $.getJSON("http://pokeapi.co/api/v2/pokedex/1/", function(data){
-    console.log(data);
-  });
+  // var json = $.getJSON("http://pokeapi.co/api/v2/pokedex/1/", function(data){
+  //   console.log(data);
+  // });
   var jsonName = $.getJSON("http://pokeapi.co/api/v2/pokedex/1/", function(data2){
     console.log(data2.pokemon_entries[0].pokemon_species.name);
+  });
+
+  var jsonName = $.getJSON("http://pokeapi.co/api/v2/pokemon-species/1/", function(data2){
+    console.log(data2);
   });
 });
