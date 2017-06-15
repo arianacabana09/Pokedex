a1553976d538a582932c56d65baac59cfa46e15a
Ariana Cabana <acabana.0904@gmail.com>
2017-06-15 18:11:22 -0500

    recorriendo propiedades

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,31 @@
+const state = {
+  stations: null,
+  selectedStation: null
+};
+
 $( ()=>{
-  // const search = $('#search');
 
-  // $.getJSON("assets/js/endpoint-all.js", function(data) {
-  //   console.log(data);
+  var json = $.getJSON("http://pokeapi.co/api/v2/pokemon-species/1/", function(data){
+    console.log(data);
+  });
 
-  // var json = $.getJSON("http://pokeapi.co/api/v2/pokedex/1/", function(data){
-  //   console.log(data);
-  // });
+  var json = $.getJSON("http://pokeapi.co/api/v2/pokedex/1/", function(data){
+    console.log(data);
+  });
+
+  //nombre
   var jsonName = $.getJSON("http://pokeapi.co/api/v2/pokedex/1/", function(data2){
     console.log(data2.pokemon_entries[0].pokemon_species.name);
   });
-
+// descriopcion
   var jsonName = $.getJSON("http://pokeapi.co/api/v2/pokemon-species/1/", function(data2){
-    console.log(data2);
+    console.log(data2.flavor_text_entries[3].flavor_text);
   });
+
+  // categoria
+  var jsonName = $.getJSON("http://pokeapi.co/api/v2/pokemon-species/1/", function(data3){
+    console.log(data3.genera[2].genus);
+  });
+// weight
+
 });
