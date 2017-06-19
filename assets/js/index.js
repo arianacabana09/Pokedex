764c89507efa47ee18c3878d88b12da3f0f77d68
Ariana Cabana <acabana.0904@gmail.com>
2017-06-19 09:55:14 -0500

    Sin GH-pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,31 @@
+const render = (root) => {
+  root.empty();
+  const wrapper = $('<div class="grid-pokemon"></div>');
+
+  if(state.selectedPokemon === null){
+    wrapper.append(Gallery(_ => render(root)));
+  }
+  root.append(wrapper);
+};
+
 const state = {
-  stations: null,
-  selectedStation: null
+  pokemons:[],
+  selectedPokemon: null,
+  pokemonInformation: null
 };
 
 $( ()=>{
+  $.getJSON('http://pokeapi.co/api/v2/pokedex/1/', data =>{
+    $.each(data.pokemon_entries, (i,item) =>{
+      state.pokemons.push({id: item.entry_number, name: item.pokemon_species.name});
+      if ( i === 20 ) {
+        const root = $('.grid-pokemon');
+        render(root);
+        return false;
+      }
+    });
+
+  }).fail(() =>{alert("Error server");});
 
   var json = $.getJSON("http://pokeapi.co/api/v2/pokemon-species/1/", function(data){
     console.log(data);
